9a6734e914c91d759d3a23e0eb244165a73731ed
Uriel <imurx@proton.me>
2024-04-28 19:44:19 -0300

    add git improvements

--- modules/git.nix
+++ modules/git.nix
@@ -25,13 +25,27 @@ in
 
     config = mkIf cfg.enable {
       home-manager.users.uri = {...}: {
+        home.packages = with pkgs; [
+          git-interactive-rebase-tool
+        ];
+
         programs.git = {
           enable = true;
           package = pkgs.gitFull;
-          lfs.enable = true;
           userEmail = cfg.email;
           userName = cfg.name;
+
+          lfs.enable = true;
+          difftastic.enable = true;
+
+          aliases = {};
+
           extraConfig = {
+            # Cooler interactive rebase
+            sequence.editor = "interactive-rebase-tool";
+            # https://git-scm.com/book/en/v2/Git-Tools-Rerere
+            rerere.enabled = true;
+
             submodule = {
               recurse = true;
             };
@@ -54,6 +68,82 @@ in
             init.defaultBranch = "main";
             # Rewrite unencrypted git://github.com URLs to the encrypted version which isn't deprecated
             ${''url "git@github.com:"''} = {insteadOf = "git://github.com/";};
+
+            maintenance = {
+              repo = "/home/uri/proyects/SlimeVR-Server";
+            };
+          };
+        };
+
+        # https://github.com/nix-community/home-manager/issues/2765
+        systemd.user = {
+          services = let
+            serviceCommand = {
+              name,
+              command,
+            }: {
+              Unit = {
+                Wants = "${name}.timer";
+              };
+
+              Service = {
+                Type = "oneshot";
+                ExecStart = command;
+              };
+
+              Install = {
+                WantedBy = ["multi-user.target"];
+              };
+            };
+
+            serviceGit = {time}:
+              serviceCommand {
+                name = "git-${time}";
+                command = let
+                  git = config.programs.git.package;
+                in ("${git}/libexec/git-core/git --exec-path=${git}/libexec/git-core/ for-each-repo "
+                  + "--config=maintenance.repo maintenance run --schedule=${time}");
+              };
+          in {
+            git-hourly = serviceGit {time = "hourly";};
+            git-daily = serviceGit {time = "daily";};
+            git-weekly = serviceGit {time = "weekly";};
+          };
+
+          timers = let
+            timer = {
+              name,
+              onCalendar,
+            }: {
+              Unit = {
+                Requires = "${name}.service";
+              };
+
+              Timer = {
+                OnCalendar = onCalendar;
+                AccuracySec = "12h";
+                Persistent = true;
+              };
+
+              Install = {
+                WantedBy = ["timers.target"];
+              };
+            };
+          in {
+            git-hourly = timer {
+              name = "git-hourly";
+              onCalendar = "hourly";
+            };
+
+            git-daily = timer {
+              name = "git-daily";
+              onCalendar = "hourly";
+            };
+
+            git-weekly = timer {
+              name = "git-weekly";
+              onCalendar = "weekly";
+            };
           };
         };
       };
