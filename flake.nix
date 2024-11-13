40227341fa73a246bd5606dad2241ddb65a56484
Uriel <imurx@proton.me>
2024-11-13 23:12:46 +0100

    update deps

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     nixpkgs = {
       # Using the nixos-unstable branch specifically, which is the
       # closest you can get to following the equivalent channel with flakes.
-      url = "github:ImUrX/nixpkgs/cherry-fhs";
+      url = "github:NixOS/nixpkgs/nixos-unstable";
     };
 
     home-manager.url = "github:nix-community/home-manager";
