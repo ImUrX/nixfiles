514d1ea928db4a936a0f5409b3c81df4b8c45c77
Uriel <imurx@proton.me>
2025-05-06 05:02:43 -0300

    fix recyclarr

--- hosts/parana/arrs.nix
+++ hosts/parana/arrs.nix
@@ -6,9 +6,34 @@
     sonarr.enable = true;
 
     bazarr.enable = true;
-    recyclarr.enable = true;
     prowlarr.enable = true;
 
     jellyseerr.enable = true;
+  };
+
+  recyclarr = {
+    enable = true;
+    configuration = {
+      sonarr = {
+        series = {
+          base_url = "http://localhost:8989";
+          api_key = "!env_var SONARR_API_KEY";
+          quality_definition = {
+            type = "series";
+          };
+          delete_old_custom_formats = true;
+        };
+      };
+      radarr = {
+        movies = {
+          base_url = "http://localhost:7878";
+          api_key = "!env_var RADARR_API_KEY";
+          quality_definition = {
+            type = "movie";
+          };
+          delete_old_custom_formats = true;
+        };
+      };
+    };
   };
 }
