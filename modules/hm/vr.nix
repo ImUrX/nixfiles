157f66ba3b34ac010b6dd80176ff225bcc084e44
Uriel <imurx@proton.me>
2025-10-10 17:54:49 -0300

    improve vr config more

--- modules/hm/vr.nix
+++ modules/hm/vr.nix
@@ -14,55 +14,55 @@ with lib;
   };
 
   config = mkIf cfg.enable {
-    home.packages =
-      with pkgs;
-      [
-        # opencomposite
-        # xrizer
-        wlx-overlay-s
-        wayvr-dashboard
-        slimevr
-        eepyxr
+    home.packages = with pkgs; [
+      # opencomposite
+      # xrizer
+      wlx-overlay-s
+      wayvr-dashboard
+      slimevr
+      eepyxr
 
-        bs-manager
-      ];
+      bs-manager
+    ];
 
-    # "${pkgs.opencomposite}/lib/opencomposite"
-    # xdg.configFile."openvr/openvrpaths.vrpath" = {
-    #   source = pkgs.writeText "openvrpaths.vrpath" ''
-    #     {
-    #       "config" :
-    #       [
-    #         "~/.local/share/Steam/config"
-    #       ],
-    #       "external_drivers" : null,
-    #       "jsonid" : "vrpathreg",
-    #       "log" :
-    #       [
-    #         "~/.local/share/Steam/logs"
-    #       ],
-    #       "runtime" :
-    #       [
-    #         "${pkgs.xrizer}/lib/xrizer"
-    #       ],
-    #       "version" : 1
-    #     }
-    #   '';
-    #   # force = true;
-    # };
-
-    xdg.configFile."wlxoverlay/watch.yaml".source = with pkgs; (replaceVars ./config/wlxoverlay/watch.yaml {
-      playerctl = "${playerctl}/bin/playerctl";
-      wpctl = "${wireplumber}/bin/wpctl";
-      DEFAULT_AUDIO_SINK = null;
-    });
-    xdg.configFile."wlxoverlay/wayvr.yaml".source = with pkgs; (replaceVars ./config/wlxoverlay/wayvr.yaml {
-      wayvr = "${wayvr-dashboard}/bin/wayvr-dashboard";
-      slimevr = "${slimevr}/bin/slimevr";
-      console = "${kdePackages.konsole}/bin/konsole";
-      firefox = "${firefox}/bin/firefox";
-      cage = "${cage}/bin/cage";
-      btop = "${btop}/bin/btop";
-    });
+    xdg.configFile = {
+      "wlxoverlay/watch.yaml".source =
+        with pkgs;
+        (replaceVars ./config/wlxoverlay/watch.yaml {
+          playerctl = "${playerctl}/bin/playerctl";
+          wpctl = "${wireplumber}/bin/wpctl";
+          DEFAULT_AUDIO_SINK = null;
+        });
+      "wlxoverlay/wayvr.yaml".source =
+        with pkgs;
+        (replaceVars ./config/wlxoverlay/wayvr.yaml {
+          wayvr = "${wayvr-dashboard}/bin/wayvr-dashboard";
+          slimevr = "${slimevr}/bin/slimevr";
+          console = "${kdePackages.konsole}/bin/konsole";
+          firefox = "${firefox}/bin/firefox";
+          cage = "${cage}/bin/cage";
+          btop = "${btop}/bin/btop";
+        });
+      # "${pkgs.xrizer}/lib/xrizer"
+      "openvr/openvrpaths.vrpath".text = ''
+        {
+          "config" :
+          [
+            "~/.local/share/Steam/config"
+          ],
+          "external_drivers" : null,
+          "jsonid" : "vrpathreg",
+          "log" :
+          [
+            "~/.local/share/Steam/logs"
+          ],
+          "runtime" :
+          [
+            "${pkgs.opencomposite}/lib/opencomposite"
+          ],
+          "version" : 1
+        }
+      '';
+    };
   };
 }
