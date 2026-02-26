7cb36e997b72045bf862748b1944f2614b78ad6d
Uriel <imurx@proton.me>
2026-02-26 18:59:36 -0300

    forgot a part

--- hosts/parana/downloaders.nix
+++ hosts/parana/downloaders.nix
@@ -34,6 +34,7 @@ in
     flood.enable = true;
     messageLevel = "info";
     vpn.enable = true;
+    package = pkgs.transmission_4;
 
     # Doesn't build
     privateTrackers.cross-seed = {
@@ -65,7 +66,7 @@ in
       if [ "$protocol" = tcp ]
       then
         echo "Telling transmission to listen on peer port $new_port."
-        ${old_transmission}/bin/transmission-remote 192.168.15.1 --port "$new_port"
+        ${pkgs.transmission_4}/bin/transmission-remote 192.168.15.1 --port "$new_port"
       fi
     '';
   };
