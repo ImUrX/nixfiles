c8b1e494a7d461c8b4a3948bee1b15b3ee2f1b7e
Uriel <imurx@proton.me>
2026-05-18 21:41:19 -0400

    copilot chat 🤮

--- modules/vscode.nix
+++ modules/vscode.nix
@@ -32,7 +32,22 @@ with lib;
           davidanson.vscode-markdownlint
           redhat.vscode-yaml
           fill-labs.dependi
-          github.copilot
+          (pkgs.vscode-utils.buildVscodeMarketplaceExtension {
+            mktplcRef = {
+              publisher = "github";
+              name = "copilot-chat";
+              version = "0.44.2";
+              hash = "sha256-kjLpbA6zUta4K86yEDiLNWvy3kJ3AvF2fncCO/JVl6I=";
+            };
+
+            meta = {
+              description = "GitHub Copilot Chat is a companion extension to GitHub Copilot that houses experimental chat features";
+              downloadPage = "https://marketplace.visualstudio.com/items?itemName=GitHub.copilot-chat";
+              homepage = "https://github.com/features/copilot";
+              license = lib.licenses.mit;
+              maintainers = [ ];
+            };
+          })
           # Javascript
           dbaeumer.vscode-eslint
           esbenp.prettier-vscode
